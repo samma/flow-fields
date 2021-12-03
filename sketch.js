662e943b22f0e3e243b9bf2a5c1902a290ace51d
samme <torgeir.lien@samma.no>
2021-12-03 17:38:05 +0100

    Flowing lava

--- sketch.js
+++ sketch.js
@@ -1,28 +1,52 @@
+// Draw settings
+let screenDivisions = 1;
+let numparticles = 5000;
+let noiseScale = 0.0015;
+let particleSpeed = 10;
 
+// Backing variables
 let particles = [];
 let gradient;
-let screenDivisions = 2;
-let numparticles = 5000;
+let topology;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  background(255);
-  noiseSeed(123);
-
-  renderloop(screenDivisions);
+  background("#E26A2C");
+  frameRate(60);
 
 
-  for (var i = 0; i < numparticles; i++) {
-    particles[i] = new Point(getrandomPointOnscreen().x, getrandomPointOnscreen().y, 0.5, screenDivisions);
-  }
+  noiseSeed(1257);
+  createWorld(screenDivisions);
+  initParticles();
+
+  //drawField(topology, width / screenDivisions, height / screenDivisions, screenDivisions);
+  //drawGradient(gradient, width / screenDivisions, height / screenDivisions, screenDivisions);
+
 }
 
 function draw() {
+  drawParticles();
+}
+
+function initParticles() {
+  // Init particles
+  for (var i = 0; i < numparticles; i++) {
+    particles[i] = new Point(getrandomPointOnscreen().x, getrandomPointOnscreen().y, particleSpeed, screenDivisions);
+  }
+}
+
+function drawParticles()  {
   // Iterate over particles
   for (var i = 0; i < particles.length; i++) {
-    particles[i].update(gradient);
-    particles[i].display();
+    if (particles[i].isAlive(gradient) ){
+      particles[i].update(gradient);
+      particles[i].display();
+    } else {
+      // TODO remove the dead particle for performance
+      particles[i] = new Point(getrandomPointOnscreen().x, getrandomPointOnscreen().y, particleSpeed, screenDivisions); 
+    }
   }
 }
 
@@ -30,13 +54,11 @@ function getrandomPointOnscreen() {
   return createVector(random(width), random(height));
 }
 
-function renderloop(screenDivisions) {
-  var topology = generateTopology(width / screenDivisions, height / screenDivisions);
-  topology = addPerlinNoise(topology, width / screenDivisions, height / screenDivisions, 0.01);
-  drawField(topology, width / screenDivisions, height / screenDivisions, screenDivisions);
+function createWorld(screenDivisions) {
+  topology = generateTopology(width / screenDivisions, height / screenDivisions);
+  topology = addPerlinNoise(topology, width / screenDivisions, height / screenDivisions, noiseScale);
   
   gradient = calculateGradient(topology, width / screenDivisions, height / screenDivisions);
-  //drawGradient(gradient, width / screenDivisions, height / screenDivisions, screenDivisions);
 }
 
 class Point {
@@ -45,17 +67,22 @@ class Point {
     this.y = y;
     this.speed = speed;
     this.screenDivisions = screenDivisions;
+    this.previousX = 0;
+    this.previousY = 0;
+    this.strokeWeight = random(1, 10);
+    // Set this.color to a color from a theme
+    //this.color = color(random(255), random(255), random(255));
+    this.color = getRandomColorFromPalette();
   }
 
   update(field) {
+    this.previousX = this.x;
+    this.previousY = this.y;
+
     // Round the position into a grid index
     let x = floor(this.x/this.screenDivisions);
     let y = floor(this.y/this.screenDivisions);
 
-    // Check if x or y is outside the screen
-    if (x < 0 || x >= field.length || y < 0 || y >= field[0].length) {
-      return; // TODO deallocate the Point?
-    }
 
     // Move perpendicular to gradient
     let perp = getPerpendicularVector(field[x][y])
@@ -64,23 +91,42 @@ class Point {
   }
 
   display() {
-    fill(0, 0, 255);
-    ellipse(this.x, this.y, 1, 1);
+    
+    // Draw a line from the previous position to the current position
+    
+    stroke(this.color);
+    // Draw random stroke width
+    strokeWeight(this.strokeWeight);
+
+    line(this.previousX, this.previousY, this.x, this.y);
+
+
+    //fill(10, 10, 10, 50);
+    //fill(this.color);
+    //ellipse(this.x, this.y, 1, 1);
+  }
+
+  isAlive(field) {
+    // Check if x or y is outside the screen
+
+    let x = floor(this.x/this.screenDivisions);
+    let y = floor(this.y/this.screenDivisions);
+
+    if (x < 0 || x >= field.length || y < 0 || y >= field[0].length) {
+      return false;
+    }
+    
+    if (this.previousX == this.x && this.previousY == this.y) {
+      return false;
+    }
+    
+    return true;
   }
 }
 
 function getPerpendicularVector(v) {
   return createVector(-v.y, v.x);
 }
-
-
-
-function physicsLoop() {
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].update();
-  }
-}
-
 
 
 function drawGradient(gradient, n, m, screenDivisions) {
@@ -161,4 +207,17 @@ function calculateGradientAt(topology, i, j, n, m) {
 function drawParticleAt(x, y, r, g, b, radius) {
   fill(r, g, b);
   ellipse(x, y, radius,radius);
+}
+
+
+
+
+// Create a color palette from these colors #554a35 #e10032 #ffc363 #1c4508
+function getRandomColorFromPalette() {
+  var palette = [color(85, 74, 53), color(225, 0, 50), color(255, 195, 99), color(28, 68, 8)];
+
+  // Create palette from these colors #E26A2C #FF8243 #FDA65D #FFD07F
+  var palette2 = [color(226, 106, 44), color(255, 130, 67), color(253, 168, 93), color(255, 208, 127)];
+
+  return palette2[Math.floor(Math.random() * palette2.length)];
 }
