841c244336077c95e1383ef15f701f574f8625ff
samme <torgeir.lien@samma.no>
2021-12-15 19:29:14 +0100

    Some refactoring

--- sketch.js
+++ sketch.js
@@ -1,61 +1,52 @@
-// Creates a flow field and displays it with some moving particles
-// Changes during rendering work: //0, 5 (60 fps half speed particles), 20, 23, 37 (groupOfPictures increased), 56 (screnshotting at 1000 now),
-// 73 (Added saving of settings), 61 more settings, more fields generated etc.
-// 768 (set green background color for Signe)
-// 808 (set random background colors)
-
-let fields = [];
-let canvasSize;
-let generateRandom = true;
-
-const targetNumOfPaintingsToGenerate = 1250;
-let defaultseed = 808; 
-
-
-let enableSaveThumbnail = true;
-let enableSaveTenSecondVideo = true;
+// For generating flow fields and store video.
 
 let projectName = "Flow-Fields-";
 
+// Flow field settings
+const targetNumOfPaintingsToGenerate = 3
+let startSeed = 0 
+const numVideosToGenerate = targetNumOfPaintingsToGenerate-startSeed; // Total number of fields to generate
+
+// Video and thumbnail capture settings
+let enableSaveThumbnail = true;
+let enabledSaveVideos = true;
 const frate = 60; // frame per second animated. Can be set high?
 const videofrate = 60; // Output video
-
 const numSecondsToCapture = 16;
-const numFrames = videofrate*numSecondsToCapture; // num of frames to record
-const numSecondsToSkipAtStart = 0.5;
+const numberOfFramesToRecord = videofrate*numSecondsToCapture; // num of frames to record
+const numSecondsToSkipAtStart = 0.5; // Skip some at the start, to avoid boring thumbnails at the start
 const numFramesToSkipAtStart = videofrate*numSecondsToSkipAtStart;
 
-const numFieldsToGenerate = targetNumOfPaintingsToGenerate-defaultseed;
-
+let fields = [];
+let canvasSize = 800;
 var frameCount = 0;
 
 // Like a constructor for the visualization
 function setup() {
-
-  canvasSize = min(800, 800);
   createCanvas(canvasSize, canvasSize);
   frameRate(frate);
   colorMode(HSB);
   noStroke();
 
-  renderVideos(numFieldsToGenerate, defaultseed).then(() => { console.log("Done end of setup"); });
+  renderVideos(numVideosToGenerate, startSeed).then(() => { console.log("Done end of setup"); });
 }
 
-async function renderVideos(n, defaultseed) {
+// Reset canvas between videos
+function resetCanvas() {
+  fields = [];
+  noStroke();
+  background(0);
+}
+
+async function renderVideos(numVideosToGenerate, defaultseed) {
   let seed = defaultseed;
-  for(let i = 0; i < n; i++) {
+  for(let i = 0; i <= numVideosToGenerate; i++) {
     // render video and wait until it is finished before continuing the loop
-    await new Promise(doneRecording => window.recordVideos(1, seed, doneRecording));
+    await new Promise(doneRecording => window.recordVideos(seed, doneRecording));
     seed = seed + 1;
-
-    // Clea old data
-    fields = [];
-    // clear the canvas
-    noStroke();
-
+    resetCanvas();
   }
 }
-
 
 function anim() {
   // Draw the flow field
@@ -64,10 +55,10 @@ function anim() {
   }
 }
 
-async function recordVideos(n, seed, doneRecording) {
+async function recordVideos(seed, doneRecording) {
     console.log("Recording seed: " + seed);
-    createFlowFieldWithRandomSettings(generateRandom, seed);
-    await new Promise(finRender => recordVideoUntilFrame(numFrames, seed, numFramesToSkipAtStart, finRender));
+    createFlowFieldWithRandomSettings(seed);
+    await new Promise(finRender => recordVideoUntilFrame(numberOfFramesToRecord, seed, numFramesToSkipAtStart, finRender));
     console.log("Done recording seed: " + seed);
     doneRecording();
 }
@@ -86,15 +77,16 @@ async function recordVideoUntilFrame(numFrames, seed, numFramesToSkipAtStart, fi
       for (let frameCount = 0; frameCount < numFramesToSkipAtStart+numFrames; frameCount++) {
         anim();
         if (frameCount >= numFramesToSkipAtStart) {
-          saveThumbnail(seed, frameCount, numFramesToSkipAtStart+numFrames-1);
-
+          if (enableSaveThumbnail) {
+            saveThumbnail(seed, frameCount, numFramesToSkipAtStart+numFrames-1);
+          }
           encoder.addFrameRgba(drawingContext.getImageData(0, 0, canvasSize, canvasSize).data)
           await new Promise(resolve => window.requestAnimationFrame(resolve))
         }
       }
 
       encoder.finalize()
-      if (enableSaveTenSecondVideo) {
+      if (enabledSaveVideos) {
           const uint8Array = encoder.FS.readFile(encoder.outputFilename);
           const anchor = document.createElement('a');
           anchor.href = URL.createObjectURL(new Blob([uint8Array], { type: 'video/mp4' }));
@@ -107,10 +99,9 @@ async function recordVideoUntilFrame(numFrames, seed, numFramesToSkipAtStart, fi
   })
 }
 
-
 function saveThumbnail(seed, frameCount,lastFrame) {
   saveThumbnailAtFrame(100, seed, frameCount);
-  saveThumbnailAtFrame(lastFrame, seed, frameCount); 
+  saveThumbnailAtFrame(lastFrame, seed, frameCount);   
 }
 
 function saveThumbnailAtFrame(frameToSave, seed, frameCount) {
@@ -119,128 +110,81 @@ function saveThumbnailAtFrame(frameToSave, seed, frameCount) {
   }
 }
 
-
-
-function createFlowFieldWithRandomSettings(generateRandomSettings, seed) {
-  // Create a border around the canvas
-
+function createFlowFieldWithRandomSettings(seed) {
   randomSeed(seed);
   noiseSeed(seed);
+  
+  // Equal chance to create a border or not
+  let drawBorders = true;
+  let border = drawBorders ? canvasSize/45 : 0;
 
-  let border = canvasSize/30;
   let width = canvasSize - border*2;
-  let height = canvasSize - border*2;
+  let height = width
   let originx = border;
   let originy = border;
   
   // Settings for the actual flowfields
-  let screenDivisions = 5;
-  let numparticles = 100;
-  let noiseScale = 0.005;
-  let particleSpeed = 0.0015;
-  let normalizedSpeed = particleSpeed/noiseScale;
-  let marginBetweenFields = border/2; // Border between fields
+  let screenDivisions = floor(2*random(1,4));
+  let numberOfParticles = floor(random(20, 1500));
+  let turbulence = random(0.0001, 0.015);
+  turbulence = round(turbulence*100000)/100000;   // Round noisescale to 5 decimals
+
+  let velocity = (random(0.002, 0.03)/2)/turbulence; // Adjust particle speed to match the topology
+  velocity = round(velocity*100000)/100000;   // Round particle speed to 5 decimals
+
+  let marginBetweenFields = floor(border/2); // Border between fields
 
   // For creating multiple flow fields in same window
-  let griddivs = 5;
-  let gridSize = width/griddivs;
+  let griddivs = 1;
+  let gridSize = floor(width/griddivs);
   let gridCoordinates = createGridCoordinates(originx, originy, width, height, griddivs);
-  let palettes = Palette.generatePalettes(gridCoordinates.length, 4);
+  let palettes = Palette.generatePalettes(gridCoordinates.length, random(2,5));
 
-  //iterate over gridcoordina
-  backgroundColor = color(30, 1, 87);
-  background(backgroundColor);
+  // Create HSB color 127, 20, 71
+  backgroundColor = generateRandomHSBColor();
+  background(backgroundColor)
 
-  if (generateRandomSettings) {
+  // Write all the settings to a JSON file
+  let settings = {
+    "seed": seed,
+    "screenDivisions": screenDivisions,
+    "numberOfParticles": numberOfParticles,
+    "turbulence": turbulence,
+    "velocity": velocity,
+    "marginBetweenFields": marginBetweenFields,
+    "griddivs": griddivs,
+    "gridSize": gridSize,
+    "palettes": palettes,
+    "backgroundColor": backgroundColor,
+    "drawBorders": drawBorders,
+    "projectName": projectName,
+    "canvasSize": canvasSize,
+    "videofrate": videofrate,
+    "enableSaveTenSecondVideo": enabledSaveVideos,
+    "numFramesToSkipAtStart": numFramesToSkipAtStart,
+    "numFrames": numberOfFramesToRecord
+  };
+
+  downloadJSON(projectName + str(seed) + '-settings.json', settings);
+
+  // Print settings to console
+  console.log("Settings: ", settings);
   
-    randomSeed(seed);
-    noiseSeed(seed);
-    
-    // Equal chance to create a border or not
-    drawBorders = true;
-    border = drawBorders ? canvasSize/45 : 0;
-
-    width = canvasSize - border*2;
-    height = width
-    originx = border;
-    originy = border;
-    
-    // Settings for the actual flowfields
-    screenDivisions = 2*random(1,4);
-    numparticles = random(20, 1500);
-    noiseScale = random(0.0005, 0.007);
-    particleSpeed = random(0.002, 0.03)/2;
-    normalizedSpeed = particleSpeed/noiseScale;
-    marginBetweenFields = border/2; // Border between fields
-
-    // For creating multiple flow fields in same window
-    griddivs = floor(1);
-    gridSize = width/griddivs;
-    gridCoordinates = createGridCoordinates(originx, originy, width, height, griddivs);
-    palettes = Palette.generatePalettes(gridCoordinates.length, random(2,5));
-
-    //iterate over gridcoordina
-    // Create HSB color 127, 20, 71
-    backgroundColor = generateRandomHSBColor();
-    background(backgroundColor)
-
-    // Print all of the settings to console
-    console.log("screenDivisions" + screenDivisions);
-    console.log("numparticles: " + numparticles);
-    console.log("noiseScale: " + noiseScale);
-    console.log("particleSpeed: " + particleSpeed);
-    console.log("normalizedSpeed: " + normalizedSpeed);
-    console.log("marginBetweenFields: " + marginBetweenFields);
-    console.log("griddivs: " + griddivs);
-    console.log("gridSize: " + gridSize);
-    console.log("gridCoordinates: " + gridCoordinates);
-    console.log("palettes: " + palettes);
-    console.log("backgroundColor: " + backgroundColor);
-    console.log("drawBorders: " + drawBorders);
-    console.log("seed: " + seed);
-
-    // Write all the settings to a JSON file
-    let settings = {
-      "seed": seed,
-      "screenDivisions": screenDivisions,
-      "numparticles": numparticles,
-      "noiseScale": noiseScale,
-      "particleSpeed": particleSpeed,
-      "normalizedSpeed": normalizedSpeed,
-      "marginBetweenFields": marginBetweenFields,
-      "griddivs": griddivs,
-      "gridSize": gridSize,
-      "palettes": palettes,
-      "backgroundColor": backgroundColor,
-      "drawBorders": drawBorders,
-      "projectName": projectName,
-      "canvasSize": canvasSize,
-      "videofrate": videofrate,
-      "enableSaveTenSecondVideo": enableSaveTenSecondVideo,
-      "numFramesToSkipAtStart": numFramesToSkipAtStart,
-      "numFrames": numFrames
-    };
-
-    downloadJSON(projectName + str(seed) + '-settings.json', settings);
-
-  
-  // Download an object as a JSON file with error handling
-  function downloadJSON(filename, data) {
-    var a = document.createElement("a");
-    a.download = filename;
-    a.href = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
-    a.click();
-  }
-
-}
-
   // Create the flow fields
   for (let i = 0; i < gridCoordinates.length; i++) {
     let x = gridCoordinates[i].x;
     let y = gridCoordinates[i].y;
-    fields.push(new FlowField(x,y,gridSize,gridSize,screenDivisions,noiseScale,normalizedSpeed,numparticles,backgroundColor,palettes[i], marginBetweenFields));
+    fields.push(new FlowField(x,y,gridSize,gridSize,screenDivisions,turbulence,velocity,numberOfParticles,backgroundColor,palettes[i], marginBetweenFields));
   }
 }
+
+    // Download an object as a JSON file with error handling
+    function downloadJSON(filename, data) {
+      var a = document.createElement("a");
+      a.download = filename;
+      a.href = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+      a.click();
+    }
 
 class FlowField {
   constructor(originx, originy, width,height,screenDivisions, noiseScale, particleSpeed, numparticles, backgroundColor, palette, borderlimit) {
@@ -282,7 +226,8 @@ class FlowField {
   initParticles() {
     for (var i = 0; i < this.numparticles; i++) {
       let newLocation = getrandomPointInWindowWithBorder(this.width, this.height, this.borderlimit);
-      this.particles[i] = new Point(newLocation.x, newLocation.y, this.particleSpeed, this.screenDivisions, this.palette);
+      let newStrokeWeight = random(1, 3)*random(1, 4);
+      this.particles[i] = new Point(newLocation.x, newLocation.y, this.particleSpeed, this.screenDivisions, this.palette, newStrokeWeight);
     }
   }
   
@@ -295,7 +240,8 @@ class FlowField {
       } else {
         // TODO remove the dead particle for performance, or keep regenerating them
         let newLocation = getrandomPointInWindowWithBorder(this.width, this.height, this.borderlimit);
-        this.particles[i] = new Point(newLocation.x, newLocation.y, this.particleSpeed, this.screenDivisions, this.palette); 
+        let newStrokeWeight = random(1, 3)*random(1, 4);
+        this.particles[i] = new Point(newLocation.x, newLocation.y, this.particleSpeed, this.screenDivisions, this.palette, newStrokeWeight); 
       }
     }
   }
@@ -376,14 +322,14 @@ class FlowField {
 
 // Used for the moving dots / lines
 class Point {
-  constructor(x, y, speed, screenDivisions, palette) {
+  constructor(x, y, speed, screenDivisions, palette, strokeWeight) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.screenDivisions = screenDivisions;
     this.previousX = x+1; // If prev and currenst is equal they will, the point will be killed
     this.previousY = y+1;
-    this.strokeWeight = random(1, 3)*random(1, 4);
+    this.strokeWeight = strokeWeight;
     this.palette = palette;
 
     // Set the color to a color from a theme
@@ -406,16 +352,10 @@ class Point {
   }
 
   displayAt(originx, originy){
-    
     // Draw a line from the previous position to the current position
     stroke(this.color);
     strokeWeight(this.strokeWeight);
     line(originx + this.previousX, originy + this.previousY, originx + this.x, originy + this.y);
-
-    // Used for drawing points instead of lines
-    //fill(10, 10, 10, 50);
-    //fill(this.color);
-    //ellipse(this.x, this.y, 1, 1);
   }
 
   isAlive(field, borderlimit) {
