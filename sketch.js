a2b2b2a7bb0897647d3a8292c069d47e249b3529
Samma <Torgeir.lien@samma.no>
2022-06-23 17:59:04 +0200

    Fixed previous error

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@
 
 let projectName = "Flow-Fields-";
 
-let globalScaling = getUrlParam("scaling", 139)
+let globalScaling = getUrlParam("scaling", 1)
 
 // Flow field settings
-let startSeed = parseInt(getUrlParam("seed", 1)) //floor(random(2000,100000));
+let startSeed = parseInt(getUrlParam("seed", 139)) //floor(random(2000,100000));
 let endSeed = 1250;
 let aliasScaling = parseFloat(getUrlParam("aliasScaling", 1.0)); // render high res, then reduce res and blur for better video.
 let numVideosToGenerate = endSeed - startSeed; // Total number of fields to generate
